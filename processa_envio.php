74b55e500857522a847ff7a54d06c973443c9a65
cesar <cezargouvea@gmail.com>
2022-12-29 14:14:31 -0300

    Subindo alteração

--- processa_envio.php
+++ processa_envio.php
@@ -55,15 +55,15 @@ try {
 	//Server settings
 	//$mail->SMTPDebug = 2; //Enable verbose debug output
 	$mail->isSMTP();                                            //Send using SMTP
-	$mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+	$mail->Host = '[SMTP]';                     //Set the SMTP server to send through
 	$mail->SMTPAuth = true;                                   //Enable SMTP authentication
-	$mail->Username = 'cezargouvea@gmail.com';                     //SMTP username
-	$mail->Password = 'iddnhpehxqhfavsn';                               //SMTP password
+	$mail->Username = '[EMAIL]';                     //SMTP username
+	$mail->Password = '[SENHA]';                               //SMTP password
 	$mail->SMTPSecure = 'tls';  //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port = 587; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 	
 	//Recipients
-	$mail->setFrom('cezargouvea@gmail.com', 'Teste');
+	$mail->setFrom('EMAIL]', 'Teste');
 	$mail->addAddress($mensagem->__get('para'), 'Teste');     //Add a recipient
 	// $mail->addAddress('ellen@example.com');               //Name is optional
 	//$mail->addReplyTo('info@example.com', 'Information');
